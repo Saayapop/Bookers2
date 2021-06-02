84afd13d6af3d2889528494e9ce3894961404b69
EC2 Default User <sekine.saaya.infratop@gmail.com>
2021-06-02 15:51:52 +0000

    0603

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @book = Book.new
   end
@@ -6,22 +10,48 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "successfully created."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+
   end
 
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully updated."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
+  end
+
+  def correct_user
+   @book = Book.find(params[:id])
+   if @book.user != current_user
+     redirect_to user_path(current_user)
+   end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -22,6 +27,13 @@ class UsersController < ApplicationController
       render :edit
     end
 
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   private
